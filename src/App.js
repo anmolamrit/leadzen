3a219e9ce525606ab350cfa1b0bcc0e04a470508
anmolamrit <124382435+anmolamrit@users.noreply.github.com>
2023-02-03 22:08:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ItemListScreen from './Screens/ItemListScreen';
+
+const App = () => {
+  return (
+    <>
+      <ItemListScreen />
+    </>
+  );
+};
+
+export default App;
